fb6236b89836554725b56ca241e60707218b6ae8
Ingo Ruhnke <grumbel@gmail.com>
2001-11-29 11:36:34 +0000

    Adding some missing files

---
+++ src/Ambulance.hxx
@@ -0,0 +1,43 @@
+//  $Id: Ambulance.hxx,v 1.1 2001/11/29 11:36:34 grumbel Exp $
+// 
+//  Pingus - A free Lemmings clone
+//  Copyright (C) 2000 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef AMBULANCE_HXX
+#define AMBULANCE_HXX
+
+#include <ClanLib/core.h>
+#include <ClanLib/display.h>
+#include "GameObj.hh"
+
+extern CL_ResourceManager* resources;
+
+class Ambulance : public GameObj
+{
+private:
+  CL_Surface sur;
+public:
+  Ambulance (boost::dummy_ptr<GameWorld>  w);
+
+  // Draw the object onto the screen
+  void draw (View* view);
+   
+};
+
+#endif
+
+/* EOF */
