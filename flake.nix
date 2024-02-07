d69c99b0adb9b891bbcbaa2a04242369896b0f10
Ingo Ruhnke <grumbel@gmail.com>
2024-02-07 18:02:02 +0100

    Update to nixos-23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Tank Battle Game";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     clanlib.url = "github:grumbel/clanlib-1.0";
@@ -26,7 +26,7 @@
 
              nativeBuildInputs = with pkgs; [
                cmake
-               pkgconfig
+               pkg-config
              ];
 
              buildInputs = with pkgs; [
